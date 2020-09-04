d1053268d4132bf9a3940dd72dbdb0401790d8f3
Lucas Silva <lucas.silva@dotzmkt.com.br>
2020-09-04 15:20:34 -0300

    Setup and learn basic hook usage

--- module-7/src/App.js
+++ module-7/src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>app</h1>;
+  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+
+  function handleChange(e) {
+    setNewTech(e.target.value);
+  }
+
+  return (
+    <>
+      <ul>
+        {tech.map((t) => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <input value={newTech} onChange={handleChange} />
+      <button onClick={handleAdd} type="button">
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;
