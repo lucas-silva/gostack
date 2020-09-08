06e62633c18af3bc6cb269769f57e0eef2531683
Lucas Silva <lucas.silva@dotzmkt.com.br>
2020-09-08 13:47:06 -0300

    Setup faker and factory-girl

--- module-11/server/__tests__/integration/user.test.js
+++ module-11/server/__tests__/integration/user.test.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 import User from '../../src/app/models/User';
 import truncate from '../util/truncate';
+import factory from '../factories';
 
 describe('User', () => {
   beforeEach(async () => {
@@ -10,45 +11,32 @@ describe('User', () => {
   });
 
   it('should be able to register', async () => {
+    const userAttrs = await factory.attrs('User', { password: '1234' });
+
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'tester boy',
-        email: 'tester@boy.com',
-        password: '1234',
-      });
+      .send(userAttrs);
 
     expect(response.body).toHaveProperty('id');
   });
 
   it('should not be able to register with duplicated email', async () => {
+    const userAttrs = await factory.attrs('User', { password: '1234' });
     const responseA = await request(app)
       .post('/users')
-      .send({
-        name: 'duplicate boy',
-        email: 'duplicate@boy.com',
-        password: '1234',
-      });
+      .send(userAttrs);
 
     expect(responseA.body).toHaveProperty('id');
 
     const responseB = await request(app)
       .post('/users')
-      .send({
-        name: 'duplicate boy',
-        email: 'duplicate@boy.com',
-        password: '1234',
-      });
+      .send(userAttrs);
 
     expect(responseB.status).toBe(400);
   });
 
   it('should encrypt user password when new user created', async () => {
-    const user = await User.create({
-      name: 'tester boy',
-      email: 'tester@boy.com',
-      password: '1234',
-    });
+    const user = await factory.create('User', { password: '1234' });
 
     const compareHash = await bcrypt.compare('1234', user.password_hash);
 
