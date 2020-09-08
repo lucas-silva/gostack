5cea3465497dd346ef8815f99ee3d9e44df009ea
Lucas Silva <lucas.silva@dotzmkt.com.br>
2020-09-08 13:30:39 -0300

    Added duplicated check test

--- module-11/server/__tests__/integration/user.test.js
+++ module-11/server/__tests__/integration/user.test.js
@@ -1,7 +1,13 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +18,27 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    const responseA = await request(app)
+      .post('/users')
+      .send({
+        name: 'duplicate boy',
+        email: 'duplicate@boy.com',
+        password_hash: '1234',
+      });
+
+    expect(responseA.body).toHaveProperty('id');
+
+    const responseB = await request(app)
+      .post('/users')
+      .send({
+        name: 'duplicate boy',
+        email: 'duplicate@boy.com',
+        password_hash: '1234',
+      });
+
+    expect(responseB.status).toBe(400);
   });
 });
--- module-11/server/src/app/controllers/UserController.js
+++ module-11/server/src/app/controllers/UserController.js
@@ -2,8 +2,12 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    if (req.body.email === 'hacker@google.com') {
-      return res.status(400).json({ error: 'We gotcha you' });
+    const { email } = req.body;
+
+    const checkEmail = await User.findOne({ where: { email } });
+
+    if (checkEmail) {
+      return res.status(400).json({ error: 'Duplicated email' });
     }
 
     const user = await User.create(req.body);
