f7166b08437a8e32a2ecdff730d44029a3a072c4
Lucas Silva <lucas.silva@dotzmkt.com.br>
2020-09-08 18:36:28 -0300

    Added cached

--- module-15/src/app/controllers/ProviderController.js
+++ module-15/src/app/controllers/ProviderController.js
@@ -1,9 +1,17 @@
 import User from '../models/User';
 import File from '../models/File';
 
+import Cache from '../../lib/Cache';
+
 class ProviderController {
   async index(req, res) {
-    const users = await User.findAll({
+    const cached = await Cache.get('providers');
+
+    if (cached) {
+      return res.json(cached);
+    }
+
+    const providers = await User.findAll({
       where: { provider: true },
       attributes: ['id', 'name', 'email', 'avatar_id'],
       include: [
@@ -11,7 +19,9 @@ class ProviderController {
       ],
     });
 
-    return res.json(users);
+    await Cache.set('providers', providers);
+
+    return res.json(providers);
   }
 }
 
