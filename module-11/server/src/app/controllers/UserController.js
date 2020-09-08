b276ebbeb955ec31a6b35151b747f874adac5b9f
Lucas Silva <lucas.silva@dotzmkt.com.br>
2020-09-08 11:41:23 -0300

    Added untested code to explore the coverage report

--- module-11/server/src/app/controllers/UserController.js
+++ module-11/server/src/app/controllers/UserController.js
@@ -2,6 +2,10 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    if (req.body.email === 'hacker@google.com') {
+      return res.status(400).json({ error: 'We gotcha you' });
+    }
+
     const user = await User.create(req.body);
 
     return res.json(user);
